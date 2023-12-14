f93b1d8bf16f306ce51876fcda59f6a0fd2260a3
dhruv dogra <151560644+DhruvDex@users.noreply.github.com>
2023-12-14 21:48:21 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -12,21 +12,56 @@ describe("Todolist Test Suite", () => {
             }
         );
     })
-    Test("Should add new todo", () => {
-        const todoItemsCount = all.length;
-        add(
-            {
-                title: "Test todo",
-                completed: false,
-                dueDate: new Date().toLocaleDateString("en-CA")
-            }
-        );
-        expect(all.length).toBe(todoItemsCount + 1);
+  test("Checks creating a new todo", () => {
+    const itemCount = all.length;
+    // expect(all.length).toBe(itemCount)
+    add({
+      title: "Test todo2",
+      completed: false,
+      dueDate: new Date().toISOString().slice(0, 10),
     });
+    expect(all.length).toBe(itemCount + 1);
+  });
 
-    test("Shouldd mark a todo as complete", () => {
-        expect(all[0].completed).toBe(false);
-        markAsComplete(0);
-        expect(all[0].completed).toBe(true);
-    })
-})
+  test("Checkd markinga a todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+
+  test("Checks retrival of overdue items", () => {
+    const itemCount = overdue().length;
+    // expect(overdue().length).toBe(itemCount)
+    add({
+      title: "Test todo2 yesterday",
+      completed: false,
+      dueDate: new Date(new Date().setDate(new Date().getDate() - 1))
+        .toISOString()
+        .split("T")[0],
+    });
+    expect(overdue().length).toBe(itemCount + 1);
+  });
+
+  test("Checks retrival of dueToday items", () => {
+    const itemCount = dueToday().length;
+    // expect(dueToday().length).toBe(itemCount)
+    add({
+      title: "Test todo3",
+      completed: false,
+      dueDate: new Date().toISOString().slice(0, 10),
+    });
+    expect(dueToday().length).toBe(itemCount + 1);
+  });
+
+  test("Checks retrival of dueLater items", () => {
+    const itemCount = dueLater().length;
+    // expect(dueLater().length).toBe(itemCount)
+    add({
+      title: "Test todo2 tomorrow",
+      completed: false,
+      dueDate: new Date(new Date().setDate(new Date().getDate() + 1))
+        .toISOString()
+        .split("T")[0],
+    });
+    expect(dueLater().length).toBe(itemCount + 1);
+  });
